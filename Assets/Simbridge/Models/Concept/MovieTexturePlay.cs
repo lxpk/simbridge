9a7a84f5b8346d8056c4e3f6256a5bba86df3a2a
Alex Peake <alex@primerlabs.com>
2015-05-10 14:02:11 -0700

    latest

--- Assets/Simbridge/Models/Concept/MovieTexturePlay.cs
+++ Assets/Simbridge/Models/Concept/MovieTexturePlay.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class MovieTexturePlay : MonoBehaviour {
+#if UNITY_STANDALONE_OSX
     public MovieTexture movTexture;
     void Start() {
         GetComponent<Renderer>().material.mainTexture = movTexture;
 		movTexture.loop = true;
         movTexture.Play();
     }
+#endif
 }
